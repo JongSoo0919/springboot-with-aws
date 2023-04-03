dbef2ecafb1dd7b263949d760cb6a891b30f3049
JongSooKim <prjs12@naver.com>
2023-04-03 22:41:00 +0900

    3장 JPA - JPA 코드 및 테스트 코드 작성

---
+++ src/main/java/com/swa/admin/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.swa.admin.web.dto;
+
+import com.swa.admin.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
---
+++ src/test/java/com/swa/admin/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.swa.admin.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostsRepositoryTest {
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanUp(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "게시글 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("prjs3643@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        System.out.println("createDate = " + posts.getCreatedDate());
+        System.out.println("modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
