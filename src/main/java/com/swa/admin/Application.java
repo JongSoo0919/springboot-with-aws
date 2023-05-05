42ec108c496344f097cf5a6521c19a052beb2ec6
JongSooKim <prjs3643@gmail.com>
2023-05-06 01:23:31 +0900

    9장. 코드가 푸시되면 자동으로 배포하기  - 자동배포설정

--- src/main/java/com/swa/admin/Application.java
+++ src/main/java/com/swa/admin/Application.java
@@ -2,13 +2,16 @@ package com.swa.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 //@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
     //TEST COMMIT
 }
